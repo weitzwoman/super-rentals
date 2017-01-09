5fcb40c071b8a55ad3136fccc085c38a272a53c1
Tracie Weitzman <tlweitzman@gmail.com>
2017-01-09 12:07:29 -0800

    add update for many or single values of rentals

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -8,8 +8,11 @@ export default Ember.Component.extend({
     },
     imageHide: function() {
      this.set('isImageShowing', false);
-   },
-   delete(rental) {
+    },
+    update(rental, params) {
+      this.sendAction('update', rental, params);
+    },
+    delete(rental) {
       if (confirm('Are you sure you want to delete this rental?')) {
         this.sendAction('destroyRental', rental);
       }
