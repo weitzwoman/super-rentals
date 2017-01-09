1c827ca7973ff04bb1ab2403387eb93d1bb3fd05
Tracie Weitzman <tlweitzman@gmail.com>
2017-01-09 11:46:01 -0800

    Can use buttons to show individual messages

--- app/components/message-tile.js
+++ app/components/message-tile.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showMessages: false,
   actions: {
     delete(message) {
       if (confirm('Are you sure you want to delete this awesome message?')) {
         this.sendAction('destroyMessage', message);
       }
+    },
+    messageShow: function() {
+      this.set('showMessages', true);
     }
   }
 });
